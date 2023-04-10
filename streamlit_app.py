7dd29afae227cc51c2a5aa10610019d2a02dcd76
graham-viney <104896565+graham-viney@users.noreply.github.com>
2023-04-10 13:37:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@
 import streamlit
 
 streamlit.title('My Parents Healthy Diner')
+streamlit.Header('Breakfast Menu')
+streamlit.text('Fish and Chips')
+streamlit.text('Oysters')
+streamlit.text('Burgers')
+
+
